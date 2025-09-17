313f8907b108444c6d4c1b0ebc6f9af86d264b5f
Sveta <pupsadon@gmail.com>
2025-09-17 14:39:44 +0300

    task one all ready

---
+++ src/js/task-1.js
@@ -0,0 +1,12 @@
+const ulCategories = document.querySelector('#categories');
+
+const liItems = ulCategories.querySelectorAll('li.item');
+console.log(`Number of categories:${liItems.length}`);
+
+liItems.forEach(x => {
+  const h2X = x.querySelector('h2');
+  const elements = x.querySelectorAll('li').length;
+
+  console.log(`Category:${h2X.textContent}`);
+  console.log(`Elements ${elements}`);
+});
