f3e1861384e157898cdff032dc4f495ec8ff71f2
Sveta <pupsadon@gmail.com>
2025-09-20 11:23:43 +0300

    task for

--- js/task-4.js
+++ js/task-4.js
@@ -1,25 +1,12 @@
-// const loginForm = document.querySelector('.login-form');
-// loginForm.addEventListener('submit', function (event) {
-//   event.preventDefault();
-
-//   const filds = form.querySelectorAll('input, textarea, select');
-//   let emptufound = false;
-//   filds.forEach(field => {
-//     if (field.value.trim() === '') {
-//       emptufound = true;
-//     }
-//   });
-//   if (emptufound) {
-//     alert('All form fields must be filled in');
-//     return;
-//   }
-// });
-
 const formElem = document.querySelector('.login-form');
 formElem.addEventListener('submit', event => {
   event.preventDefault();
   const emailValue = formElem.elements.email.value.trim();
   const passwordValue = formElem.elements.password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+  }
 
   const formData = {
     email: emailValue,
